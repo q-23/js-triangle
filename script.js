3392950bcad108a29660b5a5c34243fb15f88d78
Aleksander Kwiatkowski <aleksander.kwiatkowski@gmail.com>
2018-04-01 13:52:10 +0200

    Add script.js file

---
+++ script.js
@@ -0,0 +1,7 @@
+// scripts.js
+
+var a = prompt('Enter base lenght of a triangle');
+var h = prompt('Enter height of a triangle');
+var triangleArea = a*h/2;
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
